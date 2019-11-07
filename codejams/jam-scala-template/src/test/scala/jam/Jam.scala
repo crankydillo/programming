262ad59f7a4b981ac5a871c9ef59d6790e2399c6
Samuel Cox <sacox@paypal.com>
2019-11-07 16:31:16 -0600

    use oneliner

--- codejams/jam-scala-template/src/test/scala/jam/Jam.scala
+++ codejams/jam-scala-template/src/test/scala/jam/Jam.scala
@@ -15,13 +15,4 @@ object Solution {
   }
 }
 
-import org.scalatest._
 
-class Spec extends FunSpec with Matchers {
-
-  describe("Something") {
-    it("should work") {
-      true should equal(false)
-    }
-  }
-}
