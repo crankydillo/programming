cc60cb6f7a5608e449426b1b430afdae365074f0
Samuel Cox <sacox@paypal.com>
2019-11-07 18:45:07 -0600

    No need for List method.

--- codejams/2019/1107/src/main/scala/Jam.scala
+++ codejams/2019/1107/src/main/scala/Jam.scala
@@ -13,13 +13,10 @@ object Solution {
     }
   }
 
-  def notifs(exps: List[Int], d: Int): Int = {
-    exps.sliding(d+1)
-      .count { window => window.last >= 2 * median(window.take(window.size - 1)) }
-  }
-
   def notifs(exps: Array[Int], d: Int): Int = {
-    notifs(exps.toList, d)
+    exps.toList
+      .sliding(d+1)
+      .count { window => window.last >= 2 * median(window.take(window.size - 1)) }
   }
 
   def main(args: Array[String]) {
@@ -27,7 +24,7 @@ object Solution {
     val rand = new java.util.Random()
 
     println(notifs(
-      (0 to 20000).toList.map { _ => Math.abs(rand.nextInt) },
+      (0 to 20000).toArray.map { _ => Math.abs(rand.nextInt) },
       2
     ))
 
